84ec4b4fe9c6cf1d39cee2796d4474927bf035d0
Raihan Ramadhani <11211069@student.itk.ac.id>
2024-07-31 11:40:55 +0800

    isostring

--- lib/format-tanggal.tsx
+++ lib/format-tanggal.tsx
@@ -2,17 +2,17 @@ const formatTanggal = (tanggal: string | Date) => {
     // Pastikan tanggal adalah objek Date
     const date = new Date(tanggal);
   
-    // Ambil komponen tanggal dan waktu
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear();
-    const   
-   hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2,   
-   '0');
+    // Konversi tanggal ke format ISO
+    const isoString = date.toISOString();
+  
+    // Ambil komponen dari string ISO
+    const [datePart, timePart] = isoString.split('T');
+    const [year, month, day] = datePart.split('-');
+    const [hours, minutes] = timePart.split(':');
   
     // Gabungkan komponen menjadi format yang diinginkan
     return `${day}/${month}/${year} ${hours}.${minutes}`;
   };
-
+  
   export default formatTanggal;
+  
