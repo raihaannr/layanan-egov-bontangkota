2f6841e06791a94ae494631c26e9bc3e7e454024
Raihan Ramadhani <11211069@student.itk.ac.id>
2024-07-31 11:17:46 +0800

    formatdateiso

--- lib/format-date.ts
+++ lib/format-date.ts
@@ -1,16 +1,22 @@
 // lib/format-date.ts
 
 const formatDateToID = (date: string | Date) => {
+  // Ensure date is a Date object
+  const dateObj = new Date(date);
+
+  // Convert to ISO string to avoid time zone issues
+  const isoString = dateObj.toISOString();
+
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
-    timeZone: 'Asia/Makassar',
-    timeZoneName: 'short'
   };
-  return new Intl.DateTimeFormat('id-ID', options).format(new Date(date));
+
+  return new Intl.DateTimeFormat('id-ID',   
+ options).format(new Date(isoString));
 };
 
 export default formatDateToID;
