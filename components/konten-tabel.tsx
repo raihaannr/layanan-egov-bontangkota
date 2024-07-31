19c86994ead9c756a8c301cc5af048b404ecd7b7
Raihan Ramadhani <11211069@student.itk.ac.id>
2024-07-31 12:10:22 +0800

    remove lineclamp

--- components/konten-tabel.tsx
+++ components/konten-tabel.tsx
@@ -70,18 +70,18 @@ const KontenTabel: React.FC<KontenTabelProps> = ({
                       <td className="px-4 py-2 text-gray-900">
                         {startIndex + index + 1}
                       </td>
-                      <td className="px-4 py-2 text-gray-900 line-clamp-1">{pemesanan.pemohon}</td>
-                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden line-clamp-1">{pemesanan.instansi}</td>
-                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden line-clamp-1">{pemesanan.ruangan}</td>
-                      <td className="px-4 py-2 text-gray-900 line-clamp-1">{pemesanan.keperluan}</td>
-                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden line-clamp-1">
+                      <td className="px-4 py-2 text-gray-900">{pemesanan.pemohon}</td>
+                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden">{pemesanan.instansi}</td>
+                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden">{pemesanan.ruangan}</td>
+                      <td className="px-4 py-2 text-gray-900">{pemesanan.keperluan}</td>
+                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden">
                         {formatTanggal(pemesanan.pinjam)}
                       </td>
-                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden line-clamp-1">
+                      <td className="px-4 py-2 text-gray-900 xl:table-cell hidden">
                         {formatTanggal(pemesanan.pinjam)}
                       </td>
-                      <td className="px-4 py-2 text-gray-900 line-clamp-1">{pemesanan.status}</td>
-                      <td className="px-4 py-2 line-clamp-1">
+                      <td className="px-4 py-2 text-gray-900">{pemesanan.status}</td>
+                      <td className="px-4 py-2">
                         <button>
                           <ActionButton id={pemesanan.id} />
                         </button>
